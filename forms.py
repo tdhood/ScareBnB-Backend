6f3283d20368e90dbb3b7a07b337c5278ff566b3
Alex Linden <alextl2004@gmail.com>
2022-07-26 11:58:36 -0700

    add forms and classmethods on user model

---
+++ forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, Optional
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+class UserEditForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[Optional()])
+    email = StringField('E-mail', validators=[Optional(), Email()])
+    # image_url = StringField(
+    #                         '(Optional) Profile Image URL',
+    #                         validators=[Optional()])
+    bio = TextAreaField('text', validators=[Optional()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class CSRFProtectForm(FlaskForm):
+    """Form just for CSRF Protection"""
--- models.py
+++ models.py
@@ -1,7 +1,5 @@
 """SQLAlchemy models for ShareBnB."""
 
-from datetime import datetime
-
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
@@ -16,6 +14,7 @@ class User(db.Model):
     id = db.Column(
         db.Integer,
         primary_key=True,
+        autoincrement=True,
     )
 
     email = db.Column(
@@ -39,11 +38,47 @@ class User(db.Model):
         db.Text,
     )
 
-    location = db.Column(
-        db.Text,
-    )
-
     password = db.Column(
         db.Text,
         nullable=False,
     )
+
+    @classmethod
+    def signup(cls, username, email, password, image_url=DEFAULT_IMAGE_URL):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If this can't find matching user (or if password is wrong), returns
+        False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
