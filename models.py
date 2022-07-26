0307a3597f47e9c91247c92359f291fa105bef53
Alex Linden <alextl2004@gmail.com>
2022-07-26 11:49:19 -0700

    add requirements, start user model

---
+++ models.py
@@ -0,0 +1,49 @@
+"""SQLAlchemy models for ShareBnB."""
+
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    # image_url = db.Column(
+    #     db.Text,
+    #     default=DEFAULT_IMAGE_URL,
+    # )
+
+    bio = db.Column(
+        db.Text,
+    )
+
+    location = db.Column(
+        db.Text,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
