e7a4283c73b882948a037ef0dfa8b34b3194f5f4
tdhood <taylordannhood@gmail.com>
2022-11-20 10:31:43 -0800

    cleaned up code

--- aws.py
+++ aws.py
@@ -5,14 +5,15 @@ from botocore.exceptions import ClientError
 import os
 from dotenv import load_dotenv
 import uuid
+
 # from filestorage import store
 
 load_dotenv()
-# ALEX_BUCKET = "share-b-n-b"
-# TAYLOR_BUCKET = "kestrelbucket"
-BUCKET = os.environ['BUCKET']
-FOLDER = 'scarebnb/'
-print('bucket name', BUCKET)
+
+BUCKET = os.environ["BUCKET"]
+FOLDER = os.environ["FOLDER"]
+print("bucket name", BUCKET)
+
 
 def upload_file(file_name, bucket=BUCKET, object_name=None):
     """Upload a file to an S3 bucket
@@ -22,21 +23,31 @@ def upload_file(file_name, bucket=BUCKET, object_name=None):
     :param object_name: S3 object name. If not specified then file_name is used
     :return: True if file was uploaded, else False
     """
-    print("bucket", bucket)
-
+    print("bucket", BUCKET)
+    print('file_name', file_name)
     # If S3 object_name was not specified, use file_name
     if object_name is None:
-        object_name = str(uuid.uuid4())
+        object_name = f"{FOLDER}/"+str(uuid.uuid4())
 
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client("s3")
 
     try:
-        response = s3_client.put_object(Body=file_name, Bucket=BUCKET, Key=object_name, ContentType='image/jpeg')
-        image = (f'https://{BUCKET}/{FOLDER}.s3.amazonaws.com/{object_name}')
+        response = s3_client.upload_file(
+            file_name,
+            BUCKET,
+            object_name,
+            # ContentType="image/jpeg",
+            # ACL='public-read'
+        )
+        print('response', response)
+        image = f"https://{BUCKET}.s3.amazonaws.com/{object_name}"
     except ClientError as e:
+        print('Image did not upload')
         logging.error(e)
         return False
-    return image
+    print('image', image, 'object_name', object_name)
+    return [image, object_name]
+
 
 # def download_file(file_name, bucket, object_name):
 #     s3 = boto3.client('s3')
@@ -47,17 +58,22 @@ def upload_file(file_name, bucket=BUCKET, object_name=None):
 #     with open('FILE_NAME', 'wb') as f:
 #         s3.download_fileobj('BUCKET_NAME', 'OBJECT_NAME', f)
 
+
 def show_images(bucket=BUCKET):
-    print('show_image')
-    s3_client = boto3.client('s3')
+    print("show_image")
+    s3_client = boto3.client("s3")
     image_urls = []
-    print('s3_client', s3_client.list_objects(Bucket=BUCKET, Prefix=(FOLDER+'/')))
+    # print("s3_client", s3_client.list_objects(Bucket=BUCKET, Prefix=(FOLDER + "/")))
     try:
-        for item in s3_client.list_objects(Bucket=BUCKET, Prefix=FOLDER)['Contents']:
-            presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': BUCKET, 'Key': item['Key']})
+        for item in s3_client.list_objects(Bucket=BUCKET, Prefix=FOLDER)["Contents"]:
+            presigned_url = s3_client.generate_presigned_url(
+                "get_object", Params={"Bucket": BUCKET, "Key": item["Key"]}
+            )
             image_urls.append(presigned_url)
-        print('image_urls', image_urls)
+        # print("image_urls", image_urls)
     except Exception as e:
         pass
-    print("[INFO] : The contents inside show_image = ", image_urls)
-    return image_urls
+    # print("[INFO] : The contents inside show_image = ", image_urls)
+    return image_urls[1:]
+
+show_images(BUCKET)
